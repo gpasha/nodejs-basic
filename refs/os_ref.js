bdf35904bf2724c92c8a9da0c09a92d6efce5e76
gpasha <g1ebikpasha@gmail.com>
2021-07-21 16:00:36 +0300

    os

---
+++ refs/os_ref.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+// Platform
+console.log(os.platform());
+
+// Architecture
+console.log(os.arch());
+
+// Info
+console.log(os.cpus());
+
+// Free mem
+console.log(os.freemem());
+
+// Total mem
+console.log(os.totalmem());
+
+// Home dir
+console.log(os.homedir());
+
+// Up time
+console.log(os.uptime());
