66b6a839364d73458c23350c76ad3b5bcd619c2d
gpasha <g1ebikpasha@gmail.com>
2021-07-21 16:41:37 +0300

    http, nodemon

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const userObj = require('./user')
+const http = require('http')
 
-console.log('user: ', userObj.user)
-userObj.sayHello()
+const server = http.createServer((req, res) => {
+    console.log('server created, req.url: ', req.url);
+
+    res.write('<h1>Hi from NodeJS!</h1>')
+    res.write('<h2>Hi from NodeJS!</h2>')
+    res.write('<h3>Hi from NodeJS!</h3>')
+    res.end(
+        `<div style="background:red; width:200px; height:200px;">
+            <h1>Test text</h1>
+        </div>`
+    )
+})
+
+server.listen(3000, () => {
+    console.log('server is listening ...');
+})
